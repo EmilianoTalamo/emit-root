252a7aaf065d4a585ef95704dbe62c4deef899be
EmilianoTalamo <emilianotalamo@gmail.com>
2017-10-11 23:53:38 -0300

    Links to array

---
+++ about/tiles-array.js
@@ -0,0 +1,111 @@
+var tile =
+[
+	{
+		"title" : "Facebook",
+		"href" : "http://facebook.com/emilianotalamo",
+		"icon" : "fb.svg"
+	},
+	{
+		"title" : "Twitter",
+		"href" : "http://twitter.com/arrobaEmit",
+		"icon" : "tw.svg"
+	},
+	{
+		"title" : "Spotify",
+		"href" : "http://open.spotify.com/user/emilianotalamo",
+		"icon" : "spoty.svg"
+	},
+	{
+		"title" : "LinkedIn",
+		"href" : "http://linkedin.com/in/emilianotalamo/",
+		"icon" : "linkedin.svg"
+	},
+	{
+		"title" : "Instagram",
+		"href" : "https://www.instagram.com/emilianotalamo/",
+		"icon" : "ig.svg"
+	},
+	{
+		"title" : "YouTube",
+		"href" : "https://www.youtube.com/channel/UCCQdlhWW73mY7FGykZhw9KQ",
+		"icon" : "yt.svg"
+	},
+	{
+		"title" : "Steam",
+		"href" : "http://steamcommunity.com/id/emilianotalamo/",
+		"icon" : "steam.svg"
+	},
+	{
+		"title" : "Pinterest",
+		"href" : "https://pinterest.com/emilianotalamo",
+		"icon" : "pinterest.svg"
+	},
+	{
+		"title" : "TVshowtime",
+		"href" : "https://www.tvshowtime.com/en/user/5476699/profile",
+		"icon" : "tvst.svg"
+	},
+	{
+		"title" : "LastFM",
+		"href" : "http://www.last.fm/user/EmilianoTalamo",
+		"icon" : "lastfm.svg"
+	},
+	{
+		"title" : "4sq/Swarm",
+		"href" : "https://foursquare.com/emilianotalamo",
+		"icon" : "swarm.svg"
+	},
+	{
+		"title" : "Tumblr",
+		"href" : "https://emilianotalamo.tumblr.com",
+		"icon" : "tumblr.svg"
+	},
+	{
+		"title" : "Yelp",
+		"href" : "https://www.yelp.com.ar/user_details?userid=W7G7480kF5S8OvUkfkya8Q",
+		"icon" : "yelp.svg"
+	},
+	{
+		"title" : "Telegram",
+		"href" : "https://web.telegram.org/#/im?p=@EmilianoTalamo",
+		"icon" : "tg.svg"
+	},
+	{
+		"title" : "GitHub",
+		"href" : "https://github.com/EmilianoTalamo/",
+		"icon" : "git.svg"
+	},
+	{
+		"title" : "Google Plus",
+		"href" : "https://plus.google.com/110606509366116572535",
+		"icon" : "gplus.svg"
+	},
+	{
+		"title" : "Snapchat",
+		"href" : "http://snapchat.com/add/emitalamo",
+		"icon" : "snap.svg"
+	},
+	{
+		"title" : "Blizzard BattleNet",
+		"href" : "https://www.overbuff.com/players/pc/EmiTalamo-1999",
+		"icon" : "bnet.svg"
+	},
+	{
+		"title" : "reddit",
+		"href" : "https://www.reddit.com/user/EmilianoTalamo/",
+		"icon" : "reddit.svg"
+	},
+	{
+		"title" : "XDA-Developers",
+		"href" : "http://forum.xda-developers.com/member.php?u=4062845",
+		"icon" : "xda.svg"
+	},
+	{
+		"title" : "AskFM",
+		"href" : "https://ask.fm/EmilianoTalamo",
+		"icon" : "ask.svg"
+	}
+	// CuriousCat
+	// Medium
+	// Stackexchange
+]
