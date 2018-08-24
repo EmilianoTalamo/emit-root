1e11721c16f95695cded4dd24b1ff3c18cc0a246
EmilianoTalamo <emilianotalamo@gmail.com>
2018-08-24 18:37:36 -0300

    Portfolio moved to an independent repo

--- portfolio/js/loading.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-	document.body.style.overflow = "auto";
-	var overlay = document.getElementById("overlay");
-	overlay.style.opacity = 0;
-	overlay.style.pointerEvents = "none";
-	setTimeout(function() {
-		document.body.removeChild(overlay); 
-	}, 1500);
-}
