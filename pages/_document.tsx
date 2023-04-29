c9b8f8e96bd42cbfe22e95c7c9d8d23b19ec2d65
Emit <emilianotalamo@gmail.com>
2023-04-29 15:50:46 -0300

    Added google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,26 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
+	const gtag = process.env.GTAG
+
 	return (
 		<Html lang="en">
-			<Head />
+			<Head>
+				<Script
+					strategy="afterInteractive"
+					src={`https://www.googletagmanager.com/gtag/js?id=${gtag}`}
+				></Script>
+				<Script id="google-analytics" strategy="afterInteractive">
+					{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${gtag}');
+        `}
+				</Script>
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
