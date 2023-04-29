b3ff4b0bd1d8b356df29b78b00a57c3d44b4baf3
Emit <emilianotalamo@gmail.com>
2023-04-29 15:08:16 -0300

    Fix for different styling after build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import type { AppProps } from 'next/app'
 import '@/styles/vendors/_emit-normalize.scss'
 import '@/styles/style.scss'
+import { StyledEngineProvider } from '@mui/material'
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+	return (
+		<StyledEngineProvider injectFirst>
+			<Component {...pageProps} />
+		</StyledEngineProvider>
+	)
 }
